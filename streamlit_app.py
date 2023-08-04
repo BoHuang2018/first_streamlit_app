2a2e03faa5dad4f76d48c67a0623b299d99ad565
BoHuang2018 <tigerhuangbo@icloud.com>
2023-08-04 14:21:47 +0200

    Improved text of title and button label about adding fruit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ streamlit.write('The user entered', fruit_choice)
 
 # streamlit.stop()  # stop the work while we troubleshoot
 
-streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 
 
 # Snowflake related functions
@@ -61,7 +61,7 @@ def get_fruit_load_list():
 
 
 # Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
